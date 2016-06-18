fbe84073c09abd6a7d00c03da93be99fbf9e3988
Eriko Kurimoto <eriko.kurimoto.729@gmail.com>
2016-06-19 00:45:12 +0900

    定数倍改善の鬼になれない

--- pagerank.cpp
+++ pagerank.cpp
@@ -3,8 +3,11 @@
 #include <map>
 #include <iomanip>
 #include <string>
+#include <unordered_map>
 
 using namespace std;
+
+#define umap unordered_map
 
 int main(void)
 {
@@ -12,25 +15,25 @@ int main(void)
 	cin >> n;
 	vector<string> member(n);
 	for (int i = 0; i < n; i++) cin >> member[i];
-	map<string, vector<string>> go;
+	umap<string, vector<string>> go;
 	
 	int m;
 	cin >> m;
+	string x, y;
 	for (int i = 0; i < m; i++) {
-		string x, y;
 		cin >> x >> y;
 		go[x].push_back(y);
 		// go[y].push_back(x); //無向グラフなら
 	}
-	map<string, double> rank_prev;
+	umap<string, double> rank_prev;
 	for (int i = 0; i < n; i++) rank_prev[member[i]] = 100;
 	int finish = 100;
 	for (int i = 0; i < finish; i++) {
-		map<string, double> rank;
+		umap<string, double> rank;
 		for (int j = 0; j < n; j++) {
+			double plus = rank_prev[member[j]] / (double)go[member[j]].size();
 			for (int k = 0; k < (int)go[member[j]].size(); k++) {
-				rank[go[member[j]][k]] += rank_prev[member[j]] / (double)go[member[j]].size();
-				// cout << rank_prev[member[j]] << " " << go[member[j]].size() << " " << rank_prev[member[j]] / (double)go[member[j]].size() << " " << rank[go[member[j]][k]] << endl;
+				rank[go[member[j]][k]] += plus;
 			}
 		}
 		cout << setw(4) << i + 1 << " ";
